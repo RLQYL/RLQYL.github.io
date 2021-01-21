bd8517995d2a3937fcb104f5ae82db5cf34d2bc2
Rick Lan <lanrick107@gmail.com>
2021-01-20 19:38:01 -0500

    Commiting files

--- searchBar.js
+++ searchBar.js
@@ -1,14 +1,15 @@
+"use strict"
 $(document).ready(function(){
-    var input = document.getElementById("bar");
-    var a_level_classes = ["CSCA67", "LINA01", "MATA31", "MATA37", "MGTA01", "MGTA02", "PSYA01", "PSYA02"];
-    var b_level_classes = ["CSCB09", "CSCB20", "CSCB63", "LINB18", "MATB24", "MATB41", "MATB42", "MATB44", "STAB52"];
-    var c_level_classes = ["CSCC01", "CSCC09", "CSCC24", "CSCC37", "CSCC43", "CSCC63", "MATC44"];
-    var d_level_classes = ["CSCD01"];
+    let input = document.getElementById("bar");
+    let a_level_classes = ["CSCA67", "LINA01", "MATA31", "MATA37", "MGTA01", "MGTA02", "PSYA01", "PSYA02"];
+    let b_level_classes = ["CSCB09", "CSCB20", "CSCB63", "LINB18", "MATB24", "MATB41", "MATB42", "MATB44", "STAB52"];
+    let c_level_classes = ["CSCC01", "CSCC09", "CSCC24", "CSCC37", "CSCC43", "CSCC63", "MATC44"];
+    let d_level_classes = ["CSCD01"];
     
     
     $(input).keyup(function(event) {
         if (event.keyCode === 13) {
-            var class_name = input.value;
+            let class_name = input.value;
             if(class_name[3].toUpperCase() === "A" || class_name[3] === "1"){
                 if(a_level_classes.includes(class_name.toUpperCase())){
                     window.location.href = `https://rlqyl.github.io/A-Level/${class_name.toUpperCase()}/${class_name.toUpperCase()}.html`;
